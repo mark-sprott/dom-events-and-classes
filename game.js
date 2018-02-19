77ee4a223f4d36ed4c95e3773b9c10143f2957e7
mark-sprott <sprotty01@gmail.com>
2018-02-20 12:32:00 +1300

    left and right mouse click colour changes added to code

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -12,18 +12,28 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
-  }
+   dots[i].addEventListener('click', makeBlue)
+   dots[i].addEventListener('dblclick', hide)
+ }
 }
 
 function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+ evt.preventDefault()
+ evt.target.classList.toggle('green')
+ updateCounts()
 }
 
-// CREATE FUNCTION makeBlue HERE
 
+// CREATE FUNCTION makeBlue HERE
+function makeBlue(evt){
+  e.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
+function hide(evt){
+  e.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -31,15 +41,26 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
+  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var dots = document.getElementsByClassName('board')[0].children
+   for (var i = 0; i < dots.length; i++){
+     if(dots[i].classList.contains('blue')){
+       totals.blue++
+     }
+     if(dots[i].classList.contains('green')){
+       totals.green++
+     }
+     if(dots[i].classList.contains('invisible')){
+       totals.invisible++
+     }
+   }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
-}
+ }
 
-function displayTotals (totals) {
-  for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
-  }
-}
+ function displayTotals (totals) {
+   for (var key in totals) {
+     document.getElementById(key + '-total').innerHTML = totals[key]
+   }
+ }
